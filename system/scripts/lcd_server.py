b6ce4cfae38834a5b8d42ef21b5c267b9c40483e
h3ct0r <h3ct0r.ml@gmail.com>
2017-03-07 12:55:39 -0300

    added message to fix timer

--- system/scripts/lcd_server.py
+++ system/scripts/lcd_server.py
@@ -174,5 +174,5 @@ if __name__ == "__main__":
     with open('/home/pi/Git/DoorAccessRpi/system/config/config.json') as data_file:
         config = json.load(data_file)
 
-    time.sleep(10)
+    time.sleep(10) # todo: Fix this
     main(config)
